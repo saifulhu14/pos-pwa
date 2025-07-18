5e227f3dc8271fa1774a4b08d53fba61789ae019
saifulhu14 <130042024+saifulhu14@users.noreply.github.com>
2025-07-19 00:08:53 +0700

    Delete web.php

--- web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-// Route utama untuk SPA
-Route::get('/{any}', function () {
-    return view('app'); // Vue SPA akan hidup di sini
-})->where('any', '.*');
